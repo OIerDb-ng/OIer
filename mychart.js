fa845b2b2fb074237622f2ac60de95d4a9a9a2a8
bytewzk <2462525363@qq.com>
2018-07-18 06:55:42 +0800

    第一次把图表搞完

--- mychart.js
+++ mychart.js
@@ -1,38 +1,15 @@
 var config = {
 	type: 'line',
 	data: {
-		labels: ['2010', '2011', '2012', '2013', '2014', '2015', '2016'],
-		datasets: [{
-			label: 'My First dataset',
-			backgroundColor: "#ee961b",
-			borderColor: "#ee961b",
-			data: [
-				1,3,0,0,2,4,3
-			],
-			fill: false,
-		}, {
-			label: 'My Second dataset',
-			fill: false,
-			backgroundColor: "#939291",
-			borderColor: "#939291",
-			data: [
-				4,1,0,0,6,2,5
-			],
-		},{
-			label: 'My Second dataset',
-			fill: false,
-			backgroundColor: "#9c593b",
-			borderColor: "#9c593b",
-			data: [
-				1,2,3,4,6,7,9
-			],
-		}]
+		labels: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'],
+		datasets: []
 	},
 	options: {
+        responsive:true,
 		maintainAspectRatio: false,
 		title: {
 			display: true,
-			text: 'Chart.js Line Chart'
+			text: '历年获奖人数'
 		},
 		tooltips: {
 			mode: 'index',
@@ -49,17 +26,41 @@ var config = {
 					display: true,
 					labelString: '年份'
 				}
-			}],
-			yAxes: [{
-				display: true,
+            }],
+            yAxes: [{
+                display: true,
 				scaleLabel: {
-					display: true,
-					labelString: '获奖人数'
-				}
-			}]
+                    display: true,
+                    labelString: '获奖人数'
+                }
+            }]
 		}
 	}
 };
+var model = [
+	{
+		label: '1=/Au',
+		backgroundColor: "#ee961b",
+		borderColor: "#ee961b",
+		data: [0,0,0,0,0,0,0,0,0],fill: false,
+	},{
+		label: '2=/Ag',
+		fill: false,
+		backgroundColor: "#939291",
+		borderColor: "#939291",
+		data: [
+			   0,0,0,0,0,0,0,0,0
+			   ],
+	},{
+		label: '3=/Cu',
+		fill: false,
+		backgroundColor: "#9c593b",
+		borderColor: "#9c593b",
+		data: [
+			   0,0,0,0,0,0,0,0,0
+			   ],
+	}
+]
 
 window.onload = function() {
 	var ctx = document.getElementById('canvas').getContext('2d');
