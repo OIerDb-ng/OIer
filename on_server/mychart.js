e97983a60d22ebbfcb8ae38aae7a313bba79c9bf
bytewzk <2462525363@qq.com>
2018-08-06 21:30:51 +0800

    changed charts back

--- on_server/mychart.js
+++ on_server/mychart.js
@@ -1,4 +1,4 @@
-/*var config = {
+var config = {
 	type: 'line',
 	data: {
 		labels: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'],
@@ -36,77 +36,26 @@
             }]
 		}
 	}
-};*/
-var config = {
-    title: {
-        text: '折线图堆叠',
-        textStyle:{color:"#fff"}
-    },
-    tooltip: {
-        trigger: 'axis'
-    },
-    legend: {
-        data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎'],
-        textStyle:{color:"#fff"}
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true,
-        borderColor:"#fff"
-    },
-    toolbox: {
-        feature: {
-            saveAsImage: {}
-        }
-    },
-    xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['周一','周二','周三','周四','周五','周六','周日']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    color: ["#ee961b","#939291","#9c593b"]
-    ,
-    series: [
-        {
-            name:'邮件营销',
-            type:'line',
-            stack: '总量',
-            data:[120, 132, 101, 134, 90, 230, 210]
-        },
-        {
-            name:'联盟广告',
-            type:'line',
-            stack: '总量',
-            data:[220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-            name:'视频广告',
-            type:'line',
-            stack: '总量',
-            data:[150, 232, 201, 154, 190, 330, 410]
-        }
-    ]
 };
-
-
 var model = [
 	{
 		label: '1=/Au',
+		backgroundColor: "#ee961b",
+		borderColor: "#ee961b",
 		data: [0,0,0,0,0,0,0,0,0],fill: false,
 	},{
 		label: '2=/Ag',
 		fill: false,
+		backgroundColor: "#939291",
+		borderColor: "#939291",
 		data: [
 			   0,0,0,0,0,0,0,0,0
 			   ],
 	},{
 		label: '3=/Cu',
 		fill: false,
+		backgroundColor: "#9c593b",
+		borderColor: "#9c593b",
 		data: [
 			   0,0,0,0,0,0,0,0,0
 			   ],
@@ -114,6 +63,6 @@ var model = [
 ]
 
 window.onload = function() {
-	var myChart = echarts.init(document.getElementById('cht'));
-	myChart.setOption(config);
+	var ctx = document.getElementById('canvas').getContext('2d');
+	window.myLine = new Chart(ctx, config);
 };
