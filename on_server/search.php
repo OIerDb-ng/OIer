8d31b4dd95efe6f8e6a4a21540aaefecbb155410
bytewzk <2462525363@qq.com>
2019-09-19 22:49:30 +0800

    Create search.php

---
+++ on_server/search.php
@@ -0,0 +1,102 @@
+<?php
+	error_reporting(0);
+	header("Access-Control-Allow-Origin: http://www.bytew.net");
+	$conn = mysqli_connect('localhost', 'THE_USERNAME', 'THE_PASSWORD',"THE_DATABASE");
+	if(!$conn) die('Could not connect: ' . mysqli_connect_error());
+	$conn->set_charset("utf8");
+	header("Content-type: text/html; charset=utf8");
+	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+		$curesult = Array();
+		if($_GET["method"] != "specific"){
+		    $q = $_GET["q"];
+			$orz = explode(" ",$_GET["q"]);
+			$got = 0;
+			foreach($orz as $cui){
+				$cui = mysqli_real_escape_string($conn,$cui);
+				if(count($curesult)==0){
+					if(preg_match("/^[a-zA-Z\s]+$/",$cui)){
+						$curi = strtolower($cui);
+						$result = mysqli_query($conn,"SELECT * FROM OIers Where pinyin = '$curi'");
+						while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+							array_push($curesult,$row);
+						}
+					}else{
+						$result = mysqli_query($conn,"SELECT * FROM OIers Where name = '$cui'");
+						while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+							array_push($curesult,$row);
+						}
+						$result = mysqli_query($conn,"SELECT * FROM OIers Where awards like '%$cui%'");
+						while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+							array_push($curesult,$row);
+						}
+					}
+				}else{
+					$newresult = Array();
+					foreach($curesult as $row){
+						if(strpos ( $row["awards"] ,  $cui )||$row["name"] == $cui ||$row["pinyin"] == $cui )
+							array_push($newresult,$row);
+					}
+					if(count($newresult)) $curesult = $newresult;
+				}
+			}
+		}else{
+		    $qustr = "SELECT * FROM OIers Where 1 = 1 ";
+			if(isset($_GET["province"])){
+				$province = mysqli_real_escape_string($conn,$_GET["province"]);
+			}else{
+				$province = "";
+			}
+			if(isset($_GET["pinyin"])){
+				$pinyin = mysqli_real_escape_string($conn,$_GET["pinyin"]);
+			}else{
+				$pinyin = "";
+			}
+			if(isset($_GET["school"])){
+				$school = mysqli_real_escape_string($conn,$_GET["school"]);
+			}else{
+				$school = "";
+			}
+			if($pinyin!="")$qustr = $qustr." and pinyin = '".$pinyin."'";
+			if($province!="")$qustr = $qustr." and awards like '%".$province."%'";
+			if($school!=""){
+				$cresult = mysqli_query($conn,"SELECT * FROM OI_school where name like \"%'".$school."'%\"");
+				$ccresult = Array();
+				while($row=mysqli_fetch_array($cresult,MYSQLI_ASSOC)){
+					array_push($ccresult,$row);
+				}
+				if(count($ccresult)==1){
+					$qustr = $qustr." and awards like \"%'school_id': ".$ccresult[0]["id"].",%\"";
+				}else{
+					$qustr = $qustr." and awards like '%".$school."%'";
+				}
+			}
+			if(isset($_GET["name"])){
+				$name = mysqli_real_escape_string($conn,$_GET["name"]);
+			}else{
+				$name = "";
+			}
+			if($name!="")$qustr = $qustr." and name = '".$name."'";
+			$year=intval($_GET["year"]);
+			if(isset($_GET["year"]) && $_GET["year"]!="")$qustr = $qustr." and year = $year";
+			
+			$result = mysqli_query($conn,$qustr);
+			while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+				array_push($curesult,$row);
+			}
+		}
+	}
+
+	$count = 0;
+	$result = Array();
+
+	if (empty($_GET["pages"])) {
+		$start = 0;
+	} else {
+		$start = ($_GET["pages"] - 1) * 20;
+	}
+	
+	$curesult = array_slice($curesult,$start,20);
+	$result["result"] = $curesult;
+	echo json_encode($result);
+	mysqli_close($conn);
+?>
