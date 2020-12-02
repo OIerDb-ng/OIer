5a8384df941d7ebed2b7d2df9dbf26fb8d2f40fc
Zhikun Wang <nocrizwang@gmail.com>
2020-12-02 14:01:41 +0800

    Merged 200+ schools

--- model/excited.py
+++ model/excited.py
@@ -1,6 +1,6 @@
 cs = []
 for i in open("school_oped.txt").readlines():
-	cs.append(i[:-1].split(','))
+    cs.append(i[:-1].split(','))
 def fndlcs2(s1, s2):
     m = [ [ 0 for x in range(len(s2)+1) ] for y in range(len(s1)+1) ]
     d = [ [ None for x in range(len(s2)+1) ] for y in range(len(s1)+1) ]
@@ -43,33 +43,43 @@ def fndlcs(s1, s2):
     return s1[p-mmax:p]
 f_file = open("fail.txt","w")
 for i in open("b.txt").readlines():
-	cc = i[:-1].split(',')
-	if len(cc)!=3:
-		print(cc)
-		exit(0)
-	mlen = -1
-	mid = -1
-	for j in range(len(cs)):
-		cu = cs[j]
-		if cc[0] != cu[0] or cc[1]!=cu[1]:
-			continue
-		for k in cu[2:]:
-			if len(fndlcs(k,cc[2])) > mlen:
-				mlen = len(fndlcs2(k,cc[2]))
-				mid = j
+    cc = i[:-1].split(',')
+    if len(cc)!=3:
+        print(cc)
+        break
+    mlen = -1
+    mid = -1
+    
+    keywd = cc[2][:]
+    l = ["中学","高中","学校","小学"]
+    if "市" in keywd:
+        keywd = keywd.split("市")[1]
+    if "省" in keywd:
+        keywd = keywd.split("省")[1]
+    for i in l:
+        keywd = keywd.replace(i,"")
+    for j in range(len(cs)):
+        cu = cs[j]
+        if cc[0] != cu[0] or cc[1]!=cu[1]:
+            continue
+        for k in cu[2:]:
+            if len(fndlcs(k,keywd)) > mlen:
+                mlen = len(fndlcs2(k,keywd))
+                mid = j
 
-	print("MERGE ",cc[2]," INTO ",cs[mid],"?")
-	if mlen<100:
-		cop = ""
-	else:
-		cop = input()
-	if cop == "y":
-		print("MERGED ",cc[2]," INTO ",cs[mid],"!")
-		cs[mid].append(cc[2])
-	else:
-		f_file.write(cc[0]+","+cc[1]+","+cc[2]+'\n')
+    print("MERGE ",cc[2]," INTO ",cs[mid],"?")
+    if mlen<2:
+        cop = ""
+    else:
+        cop = input()
+    if cop == "y":
+        print("MERGED ",cc[2]," INTO ",cs[mid],"!")
+        cs[mid].append(cc[2])
+    else:
+        f_file.write(cc[0]+","+cc[1]+","+cc[2]+'\n')
 f_file.close()
+print(2348)
 opt = open("school_new.txt","w")
 for i in cs:
-	opt.write("".join([j+"," for j in i])[:-1]+'\n')
+    opt.write("".join([j+"," for j in i])[:-1]+'\n')
 opt.close()
