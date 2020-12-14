a7480d09bb63ffaca98ba5784d6f4decda35603e
samxiaocd <139585237@qq.com>
2020-12-14 10:51:03 +0800

    Update rank.php

--- on_server/rank.php
+++ on_server/rank.php
@@ -1,9 +1,9 @@
 <?php
-	require_once 'dbinfo.php';
+    require_once 'dbinfo.php';
     error_reporting(0);
     header("Access-Control-Allow-Origin: http://www.bytew.net, http://xn--vuqs4zq3d.com");
     // $conn = mysqli_connect('localhost', 'THE_USERNAME', 'THE_PASSWORD',"THE_DATABASE");
-	$conn = mysqli_connect(DbInfo::HOST, DbInfo::USER, DbInfo::PASSWD, DbInfo::DBNAME);
+    $conn = mysqli_connect(DbInfo::HOST, DbInfo::USER, DbInfo::PASSWD, DbInfo::DBNAME);
     if(!$conn) die('Could not connect: ' . mysqli_connect_error());
     $conn->set_charset("utf8");
     header("Content-type: text/html; charset=utf8");
