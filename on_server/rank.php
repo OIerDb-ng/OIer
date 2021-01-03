0877bbd9d558ed6604a21103b8a907e4ca775837
samxiaocd <139585237@qq.com>
2021-01-03 18:29:00 +0800

    进一步简化数据库连接配置

--- on_server/rank.php
+++ on_server/rank.php
@@ -3,7 +3,7 @@
     error_reporting(0);
     header("Access-Control-Allow-Origin: http://www.bytew.net, http://xn--vuqs4zq3d.com");
     // $conn = mysqli_connect('localhost', 'THE_USERNAME', 'THE_PASSWORD',"THE_DATABASE");
-    $conn = mysqli_connect(DbInfo::HOST, DbInfo::USER, DbInfo::PASSWD, DbInfo::DBNAME);
+    $conn = DbInfo::GetMysqlConn();
     if(!$conn) die('Could not connect: ' . mysqli_connect_error());
     $conn->set_charset("utf8");
     header("Content-type: text/html; charset=utf8");
