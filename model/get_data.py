0befd33f62b3bcfd985bad4d10cf6af25c1b3111
Zhikun Wang <nocrizwang@gmail.com>
2023-08-25 07:39:01 -0500

    cleanup

--- model/get_data.py
+++
@@ -1,5 +0,0 @@
-f = open("OI_school.csv")
-op = open("mdt.txt","w")
-for i in f.readlines():
-    c = i.split('","')
-    op.write(c[-3]+','+c[-2]+','+"".join([i+',' for i in eval(c[1])])[:-1]+'\n')
